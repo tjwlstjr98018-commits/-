f9bdec3e5df4b99280d1ab809044de1bf05c9042
tjwlstjr98018-commits <tjwlstjr98018@gmail.com>
2025-12-26 20:52:17 +0900

    Add files via upload
    오늘 정리 structs는 데이터 묶음     Vector2	2차원 벡터 (방향+크기)
    position	공간상의 위치
    velocity	위치 변화량

---
+++ C 물리 연습 1일차.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+struct vector2{
+	float x;
+	float y;
+};
+
+int main(){
+	struct vector2 position = { 0.0f,0.0f };
+	struct vector2 velocity = { 1.0f,1.0f };
+	
+	float delataime = 0.016f; //약 60프레임
+
+	while (1) {
+		position.x += velocity.x * delataime;
+		position.y += velocity.y * delataime;
+		printf("pos = (%f,%f)\n", position.x, position.y);
+		getchar();
+	}
+
+	return 0;
+}
