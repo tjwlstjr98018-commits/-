2e8921151cccbd503d22c753f7fd84ef92ac4308
tjwlstjr98018-commits <tjwlstjr98018@gmail.com>
2025-12-28 02:37:51 +0900

    Add files via upload
    구조체의 정점을 찍은것 같음 + 변수 이름이 기니까 머리가 터질것 같음 내일 아침에 복습 예정
    중력 으로 프레임당 떨어짐 구현  (능지 한계로 1프레임=1엔터)

---
+++ C 물리 연습 2일차(2).cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+struct vector2 {
+	float x;
+	float y;
+};
+
+struct vector2 Add(struct vector2 a, struct vector2 b) {   // 이해 필요 요망
+	return (struct vector2) { a.x + b.x, a.y + b.y };
+}
+
+struct vector2 Mul(struct vector2 v, float s) {     // 이게 뭔지 이해해야할듯
+	return (struct vector2) { v.x* s, v.y* s };
+}
+
+int main() {
+	struct vector2 position = { 0.0f, 10.0f };  //위에서 시작
+	struct vector2 velocity = { 0.0f, 0.0f };
+	struct vector2 gravity = { 0.0f, -9.8f };
+
+	float dt = 0.016f;
+
+	while (1) {
+		velocity = Add(velocity, Mul(gravity, dt));
+		position = Add(position, Mul(velocity, dt));
+
+		if (position.y <= 0.0f) {
+			position.y = 0.0f;
+			velocity.y = 0.0f;
+		}
+		printf("y = %f\n", position.y);
+		getchar();
+	}
+	return 0;
+}
---
+++ C 물리 연습 2일차.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+struct vector2 {
+	float x;
+	float y;
+};
+
+struct vector2 addvector(struct vector2 a,struct vector2 b) {
+	struct vector2 result;
+	result.x = a.x + b.x;
+	result.y = a.y + b.y;
+	return result;
+}	
+
+struct vector2 multiplyvector(struct vector2 v, float s){
+	struct vector2 result;
+	result.x = v.x * s;
+	result.y = v.y * s;
+	return result;
+}
+
+int main() {
+	struct vector2 position = { 0.0f, 0.0f };
+	struct vector2 velocity = { 0.0f, 0.0f };
+	struct vector2 acceleration = { 0.0f, -9.8f };
+
+	float deltatime = 0.016f;
+
+	velocity = addvector(velocity, multiplyvector(acceleration, deltatime));
+	position = addvector(position, multiplyvector(velocity, deltatime));
+
+	printf("pos = (%f, %f)", position.x, position.y);
+	getchar();
+	return 0;
+}
