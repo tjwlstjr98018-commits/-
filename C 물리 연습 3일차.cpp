4409708736227213904d1cb821fe06928d609ea0
tjwlstjr98018-commits <tjwlstjr98018@gmail.com>
2025-12-29 21:38:35 +0900

    Add files via upload
    솔직히 여기부분은 C언어 공부만 주구장창 한듯
    포인터,연산자,등 주의해서 해석한것
    #include <stdio.h>
    구조체벡터2에 소수x,y를포함시킨다
    
    구조체 바디에 구조체 (벡터2위치,벡터2이동벡터,벡터2중력) //이떄 벡터2안에는 x,y소수값이 들어있으니 주의, 소수(중력) 을 포함시킨다
    
    이함수는 함수값반환X 중력에 대하여 body는 구조체 Body를 의미한다, 구조체 벡터2 소수값
    바디 안에있는 중력안에있는 x에 f안에있는x값을 더한다
    바디 안에있는 중력안에있는 Y에 f안에있는y값을 더한다
    
    이함수는 함수값반환X 물리업데이트에 대하여 body는 구조체Body를 의미하며 소수점 dt
    구조체 벡터2 가속도 포함
    가속도안에 x는 바디안에있는 중력안에있는 x 나누기 바디안에있는 질량과 같다
    가속도안에 y는 바디안에있는 중력안에있는 y 나누기 바디안에있는 질량과 같다
    
    바디안에있는 이동벡터안에있는 x 에 가속도안에있는x 곱하기 dt와 더한다
    바디안에있는 이동벡터안에있는 y 에 가속도안에있는y 곱하기 dt와 더한다
    
    바디안에있는 위치안에있는 x에 바디안에있는 이동벡터안에있는 x 곱하기 dt 와 더한다
    바디안에있는 위치안에있는 y에 바디안에있는 이동벡터안에있는 y 곱하기 dt 와 더한다
    
    바디안에있는 중력안에있는 x 는 0.0f와 같다
    바디안에있는 중력안에있는 y 는 0.0f와 같다
    
    구조체 벡터2 중력은 0.0f, -9.8f 곱하기 플레이어 안에있는 질량과 같다
    소수dt는 0.016f이다 
    
    한번 반복
    중력더하기는 플레이어의 주소, 중력
    물리업데이트는 플레이어의 주소, dt
    
    출력하다 y는 소수점과 같다 소수점은 플레이어안에있는 위치안에있는 y 이다
    입력대기
    dt는 시간의미
    (제미나이 의견
    dt (Delta Time)의 의미: 0.016f는 1초를 60으로 나눈 값($1/60$)입니다. 즉, **"0.016초 동안 물체가 얼마나 움직였는가"**를 계산하기 위해 곱해주는 아주 짧은 시간 단위입니다.body->force 초기화: 해석에서 "0.0f와 같다"라고 하신 부분은 **"다음 계산을 위해 힘을 0으로 리셋한다"**는 의미로 이해하시면 완벽합니다.)
    전체적인 흐름 요약
    준비: 플레이어(Body)를 만들고 중력(Force)을 설정한다.
    반복:
    플레이어에게 중력을 더한다 (AddForce).
    그 힘으로 가속도→속도→위치를 계산한다 (Physicsupdate).
    계산이 끝나면 다음 프레임을 위해 힘을 0으로 지운다.
    현재 높이(y)를 보여준다.

---
+++ C 물리 연습 3일차.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+
+struct Vector2 {
+	float x;
+	float y;
+};
+
+struct Body {
+	struct Vector2 position;
+	struct Vector2 velocity;
+	struct Vector2 force;
+	float mass;
+};
+
+void AddForce(struct Body* body, struct Vector2 f) {
+	body->force.x += f.x;
+	body->force.y += f.y;
+}
+
+void Physicsupdate(struct Body* body, float dt) {  // 사실 여기서부터는 뭘 의미하는지 잘 모름 ;;
+	struct Vector2 acceleration;
+	acceleration.x = body->force.x / body->mass;
+	acceleration.y = body->force.y / body->mass;
+
+	body->velocity.x += acceleration.x * dt;
+	body->velocity.y += acceleration.y * dt;
+
+	body->position.x += body->velocity.x * dt;
+	body->position.y += body->velocity.y * dt;
+
+	body->force.x = 0.0f;
+	body->force.y = 0.0f;
+};
+
+int main() {
+	struct Body player = {
+		{0.0f, 0.0f},
+		{0.0f, 0.0f},
+		{0.0f, 0.0f},
+		1.0f
+	};
+
+	struct Vector2 gravity = { 0.0f, -9.8f * player.mass };
+	float dt = 0.016f; //약60frame 의미
+
+	while (1) {
+		AddForce(&player, gravity);
+		Physicsupdate(&player, dt);
+
+		printf("y = %f\n", player.position.y);
+		getchar();
+	}
+}
