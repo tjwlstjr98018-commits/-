11afd37dd5fbd260550d45fe7caef98c8e40225e
tjwlstjr98018-commits <tjwlstjr98018@gmail.com>
2026-01-15 21:31:25 +0900

    Add files via upload
    아 어렵네요

---
+++ C 언어 물리 연습 6일차.cpp
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+
+struct Vec2 { //Vec2 = Vector2
+	float x;
+	float y;
+};
+
+struct Body {
+	struct Vec2 position;
+	struct Vec2 velocity;
+};
+
+void physicsStep(struct Body* body, float dt) {
+	const float gravity = -9.8f;
+
+	// 중력을 적.용 한다
+	body->velocity.y += gravity * dt;
+	// 위치를 적.분 한다
+	body->position.y += body->velocity.y * dt;
+	// 충돌을 구.현 한다
+	if (body->position.y < 0.0f) {
+		body->position.y = 0.0f;  // 위치를 보.정 한다
+		body->velocity.y = 0.0f;  // 속도를 멈.춘 다 (위치만 멈출시 바닥 뚫고 나감)
+	}
+}
+
+int main() {
+	struct Body player = { 0 };
+	player.position.y = 10.0f; // start y value
+
+	float accumulater = 0.0f;
+	const float FIXED_DT = 1.0f / 60.0f;
+
+	while (1) {
+		float fremeTime = FIXED_DT;
+		accumulater += fremeTime;
+
+		while (accumulater >= FIXED_DT) {
+			physicsStep(&player, FIXED_DT);
+			accumulater -= FIXED_DT;
+		}
+		
+		printf("y = %f\n", player.position.y);
+
+		if (player.position.y <= 0) {
+			return 0;
+		}
+
+	}
+	
+
+}
---
+++ C언어 물리 연습 6일차(2).cpp
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+
+struct Vec2 { 
+	float x;
+	float y;
+};
+
+struct Body {
+	struct Vec2 position;
+	struct Vec2 velocity;
+};
+
+void physicsStep(struct Body* body, float dt) {
+	const float gravity = -9.8f;
+
+	
+	body->velocity.y += gravity * dt;
+	
+	body->position.y += body->velocity.y * dt;
+	
+	if (body->position.y < 0.0f) { //변경점
+		body->position.y = 0.0f; 
+		
+		float restitution = 0.8f; //1.0 == 완전 탄성 0.0 == 완전 비탄성
+		body->velocity.y = -body->velocity.y * restitution; //바디 앞에 - 의미는 방향반전임 위 <-> 아래 여기선 아래->위 로 쓰임
+		// 벨로시티.y는 충돌직전 속도, 레스티투션은 에너지보존 비율 예시(바닥에 -10만큼 떨어짐 {레스티투션= 0.8f} +8만큼 튀어오름
+	}
+}
+
+int main() {
+	struct Body player = { 0 };
+	player.position.y = 10.0f; // start y value
+
+	float accumulater = 0.0f;
+	const float FIXED_DT = 1.0f / 60.0f;
+
+	while (1) {
+		float fremeTime = FIXED_DT;
+		accumulater += fremeTime;
+
+		while (accumulater >= FIXED_DT) {
+			physicsStep(&player, FIXED_DT);
+			accumulater -= FIXED_DT;
+		}
+
+		printf("y = %f\n", player.position.y);
+
+	}
+}
