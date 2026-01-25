a3a95ed03a042c983610be7aa100d4bf516caa23
tjwlstjr98018-commits <tjwlstjr98018@gmail.com>
2026-01-26 00:36:32 +0900

    Add files via upload
    이번엔 0번부터 99번 까지의 공이 한번에 떨어지는 걸 만들어봄 (X좌표는 같으나 Y좌표 다름) 
    반발력을 따로 안정하고 파이시스스텝에서 0,7로 한번만 씀!
    (이유 반발력없이 떨어지는것만 만든다음에 추가해서)
    아 그리고 원래 이 작업들은 과거에 물리엔진을 만들고 싶어 시작했었지만 혼자는 무리인 것 같아 물리엔진 개념만 자세하게 공부하는 작업으로 바뀌었슴(대신 원래 1달만 기간을 잡았지만 게임을 만들 수준까지로 늘어남)
    근데 확실히 재밌어진다 마지막 0좌르륵 위에서부터 나올때 아주 굳

---
+++ C언어 물리 연습 9일차.cpp
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>  //sleep 나중에 다른걸로 바꿀 수 있을떄 바꿈
+
+#define COUNT 100
+
+typedef struct {
+	float x;
+	float y;
+} Vec2;
+
+typedef struct {
+	Vec2 pos;
+	Vec2 vel;
+} Body;
+
+const float Gravity = -20.0f;
+const float DT = 0.016f;
+
+void PhysicsStep(Body* b) {
+	//속도 업뎃
+	b->vel.y += Gravity * DT;
+	//위치
+	b->pos.y += b->vel.y * DT;
+	//바닥 충돌
+	if (b->pos.y < 0) {
+		b->pos.y = 0;
+		b->vel.y = -b->vel.y * 0.7f;
+	}
+}
+
+int main() {
+	Body balls[COUNT];
+
+	//초기화 단계
+	for (int i = 0; i < COUNT; i++) {
+		balls[i].pos.x = i;
+		balls[i].pos.y = 50 + i;
+
+		balls[i].vel.x = 0;
+		balls[i].vel.y = 0;
+	}
+
+	//게임 루프
+	while (1) {
+		system("cls");
+		
+		for (int i = 0; i < COUNT; i++) {
+			PhysicsStep(&balls[i]);
+
+			printf("Ball %d : y = %.2f\n", i, balls[i].pos.y);
+		}
+		Sleep(16);
+	}
+	return 0;
+}
